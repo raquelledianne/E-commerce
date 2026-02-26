0d335a7e006bd3f97aac1efe9e27a95fd599aa4e
raquelledianne <raquelledschoolcraft@gmail.com>
2026-02-26 08:08:01 -0800

    save6

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -35,7 +35,7 @@ const Cart = ({ cart, changeQuantity, removeItem }) => {
                                             <img src={book.url} alt="" className="cart__book--img" />
                                             <div className="cart__book--info">
                                                 <span className="cart__book--title">{book.title}</span>
-                                                <span className="cart__book--price">${(book.salePrice || book.OriginalPrice).toFixed(2)}</span>
+                                                <span className="cart__book--price">${(book.salePrice || book.originalPrice).toFixed(2)}</span>
                                                 <button className="cart__book--remove" onClick={() => removeItem(book)}>Remove</button>
                                             </div>
                                         </div>
